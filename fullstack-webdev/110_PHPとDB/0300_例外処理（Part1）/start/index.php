8ba437a9f6b74520e8a9ff029a10f0fbd7a1bc6d
bookkuun <kuboyuya@outlook.jp>
2022-03-15 09:59:59 +0900

    add: 例外処理

--- fullstack-webdev/110_PHPとDB/0300_例外処理（Part1）/start/index.php
+++ fullstack-webdev/110_PHPとDB/0300_例外処理（Part1）/start/index.php
@@ -1,19 +1,16 @@
 <?php
+
 /**
  * 例外処理とは
  */
-$user = 'test_user';
-$pwd = 'pwd';
-$host = 'localhost';
-$dbName = 'test_phpdb';
-$dsn = "mysql:host={$host};port=8889;dbname={$dbName};";
-$conn = new PDO($dsn, $user, $pwd);
 
-$pst = $conn->query('select * from test_phpdb.mst_shops');
-$result = $pst->fetchAll(PDO::FETCH_ASSOC);
-
-echo '<pre>';
-print_r($result);
-echo '</pre>';
-
-$conn = null;
+try {
+    $bool = false;
+    // $bool->method();
+    echo '通常処理が実行されました。<br>';
+} catch (Error $e) {
+    echo '例外処理が実行されました。<br>';
+    echo $e->getMessage() . '<br>';
+} finally {
+    echo '終了処理が実行されました。<br>';
+}
