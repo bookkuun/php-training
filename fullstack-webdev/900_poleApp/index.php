1d151bc3dbd8f6024831b55a2c68720533cd2d50
bookkuun <kuboyuya@outlook.jp>
2022-03-18 21:39:14 +0900

    update: controllerの整理

--- fullstack-webdev/900_poleApp/index.php
+++ fullstack-webdev/900_poleApp/index.php
@@ -2,15 +2,26 @@
 require_once 'config.php';
 echo $_SERVER['REQUEST_URI'];
 
-
 require_once SOURCE_BASE . 'partials/header.php';
 
-if ($_SERVER['REQUEST_URI'] === '/poll/login') {
-    require_once SOURCE_BASE . 'controllers/login.php';
-} elseif ($_SERVER['REQUEST_URI'] === '/poll/register') {
-    require_once SOURCE_BASE . 'controllers/register.php';
-} elseif ($_SERVER['REQUEST_URI'] === '/poll/home') {
-    require_once SOURCE_BASE . 'controllers/home.php';
+$rpath = str_replace(BASE_CONTEXT_PATH, '', $_SERVER['REQUEST_URI']);
+
+route($rpath);
+
+function route($rpath)
+{
+    if ($rpath === '') {
+        $rpath = 'home';
+    }
+
+    $targetFile = SOURCE_BASE . "controllers/{$rpath}.php";
+
+    if (!file_exists($targetFile)) {
+        require_once SOURCE_BASE . "views/404.php";
+        return;
+    }
+
+    require_once $targetFile;
 }
 
 require_once SOURCE_BASE . 'partials/footer.php';
