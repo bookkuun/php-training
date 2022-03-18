42516574540d79ff038cce1a809ab91737c2eb7d
bookkuun <kuboyuya@outlook.jp>
2022-03-18 11:50:13 +0900

    add: voteApp作成開始

---
+++ fullstack-webdev/900_poleApp/config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('BASE_CONTEXT_PATH', "/poll/");
+define('BASE_IMAGE_PATH', "/poll/images/");
---
+++ fullstack-webdev/900_poleApp/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'config.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>voteApp</title>
+</head>
+
+<body>
+    <img src="<?php echo BASE_IMAGE_PATH ?>logo.svg" alt="">
+</body>
+
+</html>
